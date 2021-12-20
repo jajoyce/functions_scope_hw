fa8fffc1f5e02c3a1cfc456c4c41b0ffe9f6ca80
jajoyce <jjoyce4@gmail.com>
2021-12-19 22:42:53 -0500

    Commit 1 - Verbal Questions

---
+++ app.js
@@ -0,0 +1,6 @@
+// ## 1. Verbal questions
+
+// 1. Parameters are the variables defined when declaring a function, like placeholders. Arguments are the actual values passed into a function when calling it.
+// 2. Return actually outputs a result wherever the function is called that can then be used further, while console.log just prints the output to the console/terminal.
+// 3. A value returned by a function can be used and reused within and throughout your code wherever you would use any value, instead of e.g. only sending a message to the console. 
+
